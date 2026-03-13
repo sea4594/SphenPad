dbad9e78351d816abd42be25a662e9a99efdc2a9
Sphenpad-Dev <sphenpad@proton.me>
2026-03-13 18:29:31 +0000

    Use provided photo for app icon assets

--- src/ui/AppBrand.tsx
+++ src/ui/AppBrand.tsx
@@ -1,7 +1,7 @@
 export function AppBrand() {
   return (
     <div className="brandWrap">
-      <img className="brandIcon" src={`${import.meta.env.BASE_URL}sphen-icon.svg`} alt="" aria-hidden="true" />
+      <img className="brandIcon" src={`${import.meta.env.BASE_URL}sphen-icon-192.png`} alt="" aria-hidden="true" />
       <div className="brand">SphenPad</div>
     </div>
   );
